714df7dc4608ce74ffa8152677dfaa2247748845
NolanRS <smithn10@seattleu.edu>
2019-07-04 10:10:02 -0700

    Initial Commit: makefile and sql handler

---
+++ sql5300.cpp
@@ -0,0 +1,23 @@
+/*
+ * Project Giraffe: CPSC5300
+ * sql5300.cpp
+ * Purpose: SQL statement handler for BerkeleyDB
+ *
+ * @author Nolan Smith & Grady Diakubama
+ * @version 1.0 07/04/2019
+ *
+ * */
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include "db_cxx.h"
+#include "SQLParser.h"
+#include "sqlhelper.h"
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
